48262db55436c9f3281e70a39ae0f0e9a82fa797
Dan Batiste <danbatiste@gmail.com>
2020-04-12 13:06:46 -0700

    Add files via upload

---
+++ chem.py
@@ -0,0 +1,12 @@
+import json
+
+with open('elements.json', 'r') as file:
+    data = file.read()
+    
+elements = json.loads(data)
+moles = dict([(k, v[0]) for k, v in elements.items()])
+
+# Constants
+h = 6.62607015*10**-34    ## Planck constant (J*s)
+N0 =  6.02214076*10**23   ## Avagadro number (counting number)
+c = 299792458             ## Speed of light (m/s)
