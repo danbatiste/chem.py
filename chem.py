be6c135410817ab57f9c120c7314598d481b3351
Dan Batiste <danbatiste@gmail.com>
2020-05-22 10:53:38 -0700

    updated atomic_mass()

--- chem.py
+++ chem.py
@@ -5,7 +5,10 @@ with open('elements.json', 'r') as file:
     data = file.read()
 elements = json.loads(data)
 
-def orbitals(inpt, ion=0, output_type="include_chars"):
+def isdigit(char):
+    return char in "1234567890"
+
+def orbitals(inpt, charge=0, output_type="include_chars"):
     """Returns the orbitals of an element.
     
     inpt: The input, as either a number (34) or an element ("Se")
@@ -17,7 +20,8 @@ def orbitals(inpt, ion=0, output_type="include_chars"):
     if type(inpt) == str:
         electron_num = atomic_number(inpt)
     else:
-        electron_num = inpt + ion
+        electron_num = inpt
+    electron_num -= charge
     
     ## Generates the orbitals
     def orbital_gen():
@@ -46,18 +50,54 @@ def orbitals(inpt, ion=0, output_type="include_chars"):
         return orbital_list
     
     
-def atomic_mass(element):
+def atomic_mass(element, atom=False):
     """Returns the atomic mass given an element's symbol."""
     global elements
-    amu_dict = dict([(k, v[0]) for k, v in elements.items()])
-    return amu_dict[element]
+    if atom:
+        amu_dict = dict([(k, v[0]) for k, v in elements.items()])
+        return amu_dict[element]
+    else:
+        # Loop over molecule, break into parts
+        parts = []
+        current_part = ["",1] # ex: ["Mn", 3]
+        for i, c in enumerate(element):
+            if isdigit(c):
+                current_part[1] = int(c)
+                if i==len(element)-1 or not isdigit(element[i+1]):
+                    parts += [current_part]
+                    current_part = ["",1]
+                next
+            if not isdigit(c) and c==c.upper():
+                current_part[0] += c
+                pass_cond = i==len(element)-1
+                pass_cond = pass_cond or ((not isdigit(element[i+1])) and element[i+1] == element[i+1].upper())
+                if pass_cond:
+                    parts += [current_part]
+                    current_part = ["",1]
+                next
+            if not isdigit(c) and c==c.lower():
+                current_part[0] += c
+                pass_cond = i==len(element)-1
+                pass_cond = pass_cond or ((not isdigit(element[i+1])) and element[i+1] == element[i+1].upper())
+                if pass_cond:
+                    parts += [current_part]
+                    current_part = ["",1]
+                next
+        total_amu = 0
+        for part in parts:
+            total_amu += part[1]*atomic_mass(part[0],atom=True)
+        return total_amu
 
-
-def atomic_number(element):
+def atomic_number(element, molecule=False):
     """Returns the atomic number given an element's symbol."""
     global elements
     an_dict = dict([(k, v[2]) for k, v in elements.items()])
     return an_dict[element]
+
+            
+
+# molma("CHO", [6,12,6]) -> 
+# molma = lambda letters, nums: sum([n*amu(l) for l,n in zip(letters, nums)])
 
 
 # Constants
